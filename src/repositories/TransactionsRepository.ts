f12b46d1ea8a6e69d6a5c5fcfe449d56fef7cf19
mpgxc <mpgx5.c@gmail.com>
2020-06-28 01:25:47 -0300

    feat: challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,57 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+    income: number;
+    outcome: number;
+    total: number;
+}
+
+interface CreateTransactionDTO {
+    value: number;
+    type: 'income' | 'outcome';
+    title: string;
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+    private transactions: Transaction[];
 
-  constructor() {
-    this.transactions = [];
-  }
+    constructor() {
+        this.transactions = [];
+    }
 
-  public all(): Transaction[] {
-    // TODO
-  }
+    public all(): Transaction[] {
+        return this.transactions;
+    }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+    public getBalance(): Balance {
+        const balance = this.transactions.reduce(
+            (result: Balance, items: Transaction) => {
+                if (items.type === 'income') {
+                    result.income += items.value;
+                    result.total += items.value;
+                }
+                if (items.type === 'outcome') {
+                    result.outcome += items.value;
+                    result.total -= items.value;
+                }
 
-  public create(): Transaction {
-    // TODO
-  }
+                return result;
+            },
+            {
+                income: 0,
+                outcome: 0,
+                total: 0,
+            },
+        );
+        return balance;
+    }
+
+    public create({ value, title, type }: CreateTransactionDTO): Transaction {
+        const transaction = new Transaction({ value, title, type });
+        this.transactions.push(transaction);
+        return transaction;
+    }
 }
 
 export default TransactionsRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,38 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+    value: number;
+    type: 'income' | 'outcome';
+    title: string;
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+    private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
+    constructor(transactionsRepository: TransactionsRepository) {
+        this.transactionsRepository = transactionsRepository;
+    }
 
-  public execute(): Transaction {
-    // TODO
-  }
+    public execute({ value, type, title }: RequestDTO): Transaction {
+        const balance = this.transactionsRepository.getBalance();
+
+        if (
+            (type === 'outcome' && value > balance.total) ||
+            (type !== 'income' && type !== 'outcome')
+        ) {
+            throw Error(
+                'O Valor nao pode ser retirado pois e maior que o saldo disponivel!',
+            );
+        }
+        const transation = this.transactionsRepository.create({
+            value,
+            type,
+            title,
+        });
+
+        return transation;
+    }
 }
 
 export default CreateTransactionService;
